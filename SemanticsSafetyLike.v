4f1a76e16f8d92e45485090efec55a096e3ad679
Rob Blanco <roberto.blanco@inria.fr>
2018-07-08 17:26:28 +0200

    Sketch proof idea
    Found some difficulty trying to pose lemmas on psem and the program
    contained in lang, so writing the idea for the proof in text.

--- SemanticsSafetyLike.v
+++ SemanticsSafetyLike.v
@@ -33,6 +33,21 @@ Definition lang : level := @Build_level partial
                                         non_empty_sem.
 
 Lemma tgt_sem : semantics_safety_like lang.
+Proof.
+  unfold semantics_safety_like. simpl.
+  intros t P Hsem Hinf.
+  (*unfold psem. simpl.*)
+  (* Basic idea: if t is not in sem P, there is a prefix of t, m (here
+     signified with a dependent type for the sake of simplicity), that
+     does not belong to psem P. The argument can be that if every prefix
+     of t is in the semantics, then t itself must be, too.
+
+     Consider now the initial bad prefix m. If m is empty, choose it as
+     the minimal bad prefix and any event as the bad event: these two
+     instantiate the existentials in the goal. If m = snoc m' e, there
+     are two possible cases: if m' is in sem P, it is the minimal bad
+     prefix and e the bad event, and we are done; if m' is still not in
+     sem P, we continue reducing it. (Thus, an easy induction.) *)
 Admitted.
 
 End Main.
