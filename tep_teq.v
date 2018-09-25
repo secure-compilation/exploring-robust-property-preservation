69b5e1e564680e0e1753ea4d195ba5ab46945bc7
Carmine Abate <carmine.abate@studenti.unitn.it>
2018-09-25 15:33:20 +0200

    symmetric case in r2RTP -> TEP

--- tep_teq.v
+++ tep_teq.v
@@ -44,6 +44,21 @@ Proof.
     apply (fpr_pref_pref _ _ t2) in H0; auto.  
     destruct (same_ext (fsnoc m' i1) (fsnoc m' i2) t2); auto;
     apply snoc_m_event_equal in H1; congruence. 
-  + admit. (*symmetric case *)
-Admitted.
-  
+   + specialize (r2rpp P2 P1 traces_match).
+      assert (Hsat : rsat2 P2 P1 traces_match).
+     { intros C b1 b2 Hb1 Hb2. rewrite (H1 C b2) in Hb2. 
+       now apply (determinacy_src (C [P2])). } 
+     specialize (r2rpp Hsat Ct t1); clear Hsat.
+    assert (H : forall t2, sem tgt (Ct [P1 ↓]) t2 -> traces_match t1 t2). 
+    { intros t2 H. now apply r2rpp. } clear r2rpp H1.
+    destruct (tgt_sem (Ct [P1 ↓]) t1 k2) as [m [Hmt1 [Hpsem_m Hmax_m]]].
+    destruct Hpsem_m as [t2 [Hmt2 Hsem_t2]].
+    specialize (H t2 Hsem_t2). destruct H; try now subst.
+    destruct H as [m' [i1 [i2 [Hi1 [Hi2 [Hdiffi [Hstop' [Hpref1 Hpref2]]]]]]]].
+    assert (psem (Ct [P1 ↓]) (fsnoc m' i1)).
+    { apply (input_totality_tgt (Ct [P1 ↓]) m' i2 i1); auto. now exists t2. }
+    assert (fpr (fsnoc m' i1) m) by now apply Hmax_m.
+    apply (fpr_pref_pref _ _ t2) in H0; auto.  
+    destruct (same_ext (fsnoc m' i1) (fsnoc m' i2) t2); auto;
+    apply snoc_m_event_equal in H1; congruence.
+Qed.
