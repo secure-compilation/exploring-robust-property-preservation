4c259d2997d34d46e58489dd976929a7a8155996
Carmine Abate <carmine.abate@studenti.unitn.it>
2018-11-09 16:28:04 +0100

    RrelationalP expressed in terms of families of (hyper)properties

--- Alternative2FR.v
+++ Alternative2FR.v
@@ -13,15 +13,15 @@ Require Import Criteria.
 
 (** *Robust preservation of 2-relSafety  *)
 
-Definition R2rSP' := 
+Definition R2rSP_a := 
     forall P1 P2 S1 S2, 
     Safety S1 -> Safety S2 ->  
     (forall Cs, sat (Cs [P1]) S1 \/ sat (Cs[P2]) S2) -> 
     (forall Ct, sat (Ct [P1↓]) S1 \/ sat (Ct[P2↓]) S2).
 
 
-Definition R2rSP'_contra :
-  R2rSP' <->
+Definition R2rSP_a_contra :
+  R2rSP_a <->
   (forall P1 P2 S1 S2,
     Safety S1 -> Safety S2 ->
     (exists Ct, ~ sat (Ct [P1 ↓]) S1 /\ ~ sat (Ct [P2 ↓]) S2) ->
@@ -43,9 +43,9 @@ Proof.
     now exists Ct. exists Cs. now rewrite de_morgan2.
 Qed.   
 
-Theorem alternative_R2rSP : R2rSP' <-> R2rSP.
+Theorem alternative_R2rSP : R2rSP_a <-> R2rSP.
 Proof.
-  rewrite R2rSP'_contra, R2rSP_R2rSC.
+  rewrite R2rSP_a_contra, R2rSP_R2rSC.
   split; intros H.
   + intros Ct P1 P2 m1 m2 H1 H2.
     specialize (H P1 P2 (fun t => ~ prefix m1 t) (fun t => ~ prefix m2 t)).
@@ -90,13 +90,13 @@ Qed.
 
 (** *Robust preservation of relSafety  *)
 
-Definition RrSP' :=
+Definition RrSP_a :=
  forall (I : Type), forall (ρ : I -> par src) (δ : I -> {π | Safety π}),
      (forall Cs, exists i, sat (Cs [(ρ i)]) (proj1_sig (δ i))) ->
      (forall Ct, exists j, sat (Ct [(ρ j) ↓]) (proj1_sig (δ j))).
 
-Lemma RrSP'_contra :
-  RrSP' <->
+Lemma RrSP_a_contra :
+  RrSP_a <->
   ( forall (I : Type), forall (ρ : I -> par src) (δ : I -> {π | Safety π}),
      (exists Ct, forall i, ~ sat (Ct [(ρ i) ↓]) (proj1_sig (δ i))) -> 
      (exists Cs, forall i, ~ sat (Cs [(ρ i)]) (proj1_sig (δ i))) ). 
@@ -122,9 +122,9 @@ Proof.
 Qed.
 
 
-Theorem alternative_RrSP : RrSP <-> RrSP'.
+Theorem alternative_RrSP : RrSP <-> RrSP_a.
 Proof.
-  rewrite RrSP'_contra, RrSP_RrSC. 
+  rewrite RrSP_a_contra, RrSP_RrSC. 
   split; intro Hr.
   + intros I ρ δ [Ct Hct].
     specialize (Hr Ct  (fun P m => psem (Ct [P↓]) m) ). 
@@ -168,37 +168,180 @@ Qed.
 (* CA: 
    What follows seems stronger than RrSP 
 
-Definition RrSP' :=
+Definition RrSP_a' :=
   forall (Pf : par src -> Prop) (Sf : { π | Safety π} -> Prop),
     (forall Cs, exists P S, Pf P /\ Sf S /\ sat (Cs [P]) (proj1_sig S) ->
     (forall Ct, exists P' S', Pf P' /\ Sf S' /\ sat (Ct [P' ↓]) (proj1_sig S'))). 
 
-Lemma RrSP'_contra :
+Lemma RrSP_a'_contra :
   RrSP' <->
    forall (Pf : par src -> Prop) (Sf : { π | Safety π} -> Prop),
      (exists Ct, forall P' S', Pf P' -> Sf S' -> ~ sat (Ct [P' ↓]) (proj1_sig S')) ->
      (exists Cs, forall P S, Pf P -> Sf S -> ~ sat (Cs [P]) (proj1_sig S)).
 Admitted. 
                   
-
-Theorem alternative_RrSP : RrSP' <-> RrSP.
-Proof.
-  rewrite RrSP'_contra, RrSP_RrSC.
-  split; intros Hrel. 
-  + intros Ct f H.
-    remember (fun P => exists m, f P m) as Pf.
-    remember (fun (S : prop | Safety S) => forall P, Pf P -> ~ sat (Ct [P ↓]) (proj1_sig S)) as Sf.  
-    specialize (Hrel Pf Sf).
-    destruct Hrel as [Cs HH].
-    { exists Ct. intros P' [S' Hs'] Hp' Hsf'. 
-      rewrite HeqSf in Hsf'. specialize (Hsf' P' Hp').
-      simpl in *. assumption. }  
-    exists Cs. intros P. intros m Hm.
-    assert (Hp : Pf P). { rewrite HeqPf. now exists m. }
-              
-*)           
+*) 
                         
         
-     
-      
-  
+(** *Robust preservation of 2rel properties  *)
+
+
+Definition R2rTP_a :=
+  forall (P1 P2 : par src) (π1 π2 : prop),
+    (forall Cs, sat (Cs[P1]) π1 \/ sat (Cs[P2]) π2) ->
+    (forall Ct, sat (Ct[P1↓]) π1 \/  sat (Ct[P2↓]) π2). 
+
+Lemma R2rTP_a_contra :
+  R2rTP_a <->
+   forall (P1 P2 : par src) (π1 π2 : prop),
+    (exists Ct, ~ sat (Ct[P1↓]) π1 /\ ~ sat (Ct[P2↓]) π2) -> 
+    (exists Cs, ~ sat (Cs[P1]) π1 /\ ~ sat (Cs[P2]) π2).
+Proof.
+  split; intros Hr.
+  + intros P1 P2 π1 π2 [Ct H1].
+    rewrite <- de_morgan2 in H1.
+    specialize (Hr P1 P2 π1 π2). rewrite contra in Hr.
+    repeat rewrite not_forall_ex_not in Hr.
+    destruct Hr as [Cs Hr].
+    now exists Ct. exists Cs. now rewrite <- de_morgan2. 
+  + intros P1 P2 π1 π2. specialize (Hr P1 P2 π1 π2).
+    rewrite contra. repeat rewrite not_forall_ex_not.
+    intros [Ct Ht]. destruct Hr as [Cs Hr].
+    exists Ct. now rewrite <- de_morgan2.
+    exists Cs. now rewrite de_morgan2.
+Qed.     
+          
+Theorem alternative_R2rTP : r2RPP <-> R2rTP_a.
+Proof.
+  rewrite r2RPP_r2RC, R2rTP_a_contra.
+  split; intros Hr.
+  + intros P1 P2 π1 π2 [Ct [H1 H2]].
+    unfold sat in H1, H2. 
+    rewrite not_forall_ex_not in H1, H2.
+    destruct H1 as [t1 H1]. destruct H2 as [t2 H2].
+    rewrite not_imp in H1, H2.
+    destruct H1 as [H11 H12]. destruct H2 as [H21 H22].
+    destruct (Hr Ct P1 P2 t1 t2) as [Cs [Hc1 Hc2]]; auto.
+    exists Cs. split; unfold sat; rewrite not_forall_ex_not;
+            [exists t1 | exists t2]; firstorder.
+  + intros Ct P1 P2 t1 t2 Hsem1 Hsem2.   
+    destruct (Hr P1 P2 (fun t => t <> t1) (fun t => t <> t2)) as [Cs [H1 H2]].
+    { exists Ct. split; unfold sat; rewrite not_forall_ex_not;
+              [exists t1 | exists t2]; rewrite not_imp; now rewrite <- dne. }
+    exists Cs. unfold sat in H1, H2. rewrite not_forall_ex_not in H1, H2.
+    destruct H1 as [tt1 H1]. destruct H2 as [tt2 H2].
+    rewrite not_imp in H1, H2.
+    destruct H1 as [H11 H12]. destruct H2 as [H21 H22].
+    rewrite <- dne in H12, H22. now subst.
+Qed.
+
+
+(** *Robust preservation of 2rel hyperproperties  *)
+
+Definition R2rHP_a :=
+  forall (P1 P2 : par src) (H1 H2 : hprop),
+    (forall Cs, H1 (beh(Cs[P1])) \/ H2 (beh(Cs[P2]))) ->
+    (forall Ct, H1 (beh(Ct[P1↓])) \/ H2 (beh(Ct[P2↓]))). 
+
+Lemma R2rHP_a_contra :
+  R2rHP_a <->
+   forall (P1 P2 : par src) (H1 H2 : hprop),
+     (exists Ct, ~ H1 (beh(Ct[P1↓])) /\ ~ H2 (beh(Ct[P2↓]))) ->
+     (exists Cs, ~ H1 (beh(Cs[P1])) /\ ~ H2 (beh(Cs[P2]))).
+Proof.
+  split; intros Hr. 
+  + intros P1 P2 H1 H2. rewrite contra.
+    repeat rewrite not_ex_forall_not. 
+    specialize (Hr P1 P2 H1 H2). intros HH.
+    assert (K : (forall Cs : ctx src, H1 (beh (Cs [P1])) \/ H2 (beh (Cs [P2])))).
+    { intros Cs. specialize (HH Cs). rewrite de_morgan1 in HH.
+      repeat rewrite <- dne in HH. assumption. }
+    specialize (Hr K). intros Ct. specialize (Hr Ct).
+    rewrite de_morgan1. repeat rewrite <- dne. assumption.
+  + intros P1 P2 H1 H2. rewrite contra. repeat rewrite not_forall_ex_not.
+    specialize (Hr P1 P2 H1 H2).
+    intros [Ct HH]. destruct Hr as [Cs KK].
+    { exists Ct. now rewrite <- de_morgan2. }
+    exists Cs. now rewrite de_morgan2. 
+Qed.
+
+Theorem alternative_R2rHP : R2rHP <-> R2rHP_a.
+Proof.
+  rewrite R2rHP_R2rHC, R2rHP_a_contra. 
+  split; intros Hr.
+  + intros P1 P2 H1 H2 [Ct [HH1 HH2]].
+    destruct (Hr P1 P2 Ct) as [Cs [Heq1 Heq2]].
+    assert (beh (Ct[P1↓]) = beh (Cs[P1])). 
+    { unfold beh. now rewrite Heq1. }
+    assert (beh (Ct[P2↓]) = beh (Cs[P2])). 
+    { unfold beh. now rewrite Heq2. }
+    exists Cs. now rewrite <- H, <- H0.
+  +  intros P1 P2 Ct.
+     specialize (Hr P1 P2 (fun π => π <> beh(Ct[P1↓])) (fun π => π <> beh(Ct[P2↓]))).
+     destruct Hr as [Cs [HH1 HH2]].
+     { exists Ct. repeat rewrite <- dne. auto. }
+     rewrite <- dne in HH1, HH2.
+     now exists Cs.
+Qed.
+
+
+(** *Robust preservation of rel hyperproperties  *)
+
+Definition RrHP_a :=
+  forall (I : Type), forall (ρ : I -> par src) (δ : I -> hprop),
+     (forall Cs, exists i, (δ i) (beh (Cs [(ρ i)]))) ->
+     (forall Ct, exists j, (δ j) (beh (Ct [(ρ j)↓]))).
+
+Lemma RrHP_a_contra :
+  RrHP_a <->
+    forall (I : Type), forall (ρ : I -> par src) (δ : I -> hprop),
+        (exists Ct, forall j, ~ (δ j) (beh (Ct [(ρ j)↓]))) ->
+        (exists Cs, forall j, ~ (δ j) (beh (Cs [(ρ j)]))).
+Proof.
+  split; intros Hr.
+  + intros I ρ δ. specialize (Hr I ρ δ).
+    rewrite contra in Hr.
+    repeat rewrite not_forall_ex_not in Hr.
+    intros [Ct Ht]. destruct Hr as [Cs Hs].
+    { exists Ct. now rewrite not_ex_forall_not. }
+    exists Cs. now rewrite <- not_ex_forall_not.
+  + intros I ρ δ. specialize (Hr I ρ δ).
+    rewrite contra. 
+    repeat rewrite not_forall_ex_not. 
+    intros [Ct Ht]. destruct Hr as [Cs Hs].
+    { exists Ct. now rewrite <- not_ex_forall_not. }
+    exists Cs. now rewrite not_ex_forall_not.
+Qed. 
+
+Theorem alternative_RrHP : RrHP <-> RrHP_a.
+Proof.
+  rewrite RrHP_RrHC, RrHP_a_contra. 
+  split; intro Hr.
+  + intros I ρ δ [Ct Ht].
+    destruct (Hr Ct) as [Cs Hs].
+    assert (forall j, beh (Ct [ρ j ↓]) = beh (Cs [ρ j])).
+    { intro j. specialize (Hs (ρ j)). now unfold beh. }
+    exists Cs. intros j. now rewrite <- (H j).     
+  + intros Ct.
+    remember (fun P : (par src) => P) as ρ.  
+    remember (fun P : (par src) => (fun π => π <> (beh (Ct [P ↓])))) as δ.  
+    specialize (Hr (par src) ρ δ).
+    destruct Hr as [Cs Hs].
+    { exists Ct. intros k. rewrite Heqδ, Heqρ. tauto. }
+    exists Cs. intros P. specialize (Hs P).
+    rewrite Heqδ, Heqρ in Hs. rewrite <- dne in Hs.
+    unfold beh in Hs. firstorder.
+Qed. 
+    
+(* random remarks from previous notes 
+
+
+   a 2-rel hyperproperty is an element of 2^(beh × beh) 
+
+   if X is an infinite set, then X × X ≅ X (using AC). 
+
+   This means that 2^(beh × beh) ≅ 2^beh × 2^beh. 
+
+   The bijection above is, in general, non constructive.
+
+*)    
