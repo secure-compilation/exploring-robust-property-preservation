d899a4093335ddaceacf9c6e732670264c351202
Carmine Abate <carmine.abate@studenti.unitn.it>
2019-03-22 16:19:29 +0100

    formalize sigma tau RSCHP

--- SigmaTau.v
+++ SigmaTau.v
@@ -94,8 +94,25 @@ Definition Galois_snd (σ : @prop target -> @prop source)
 
 Definition Galois_cp  (σ : @prop target -> @prop source)
                       (τ : @prop source -> @prop target) : Prop :=
-  Galois_fst σ τ /\ Galois_snd σ τ. 
+  Galois_fst σ τ /\ Galois_snd σ τ.
 
+Definition Adjunction_law  (σ : @prop target -> @prop source)
+                           (τ : @prop source -> @prop target) : Prop := 
+
+  forall (π__s :@prop source) (π__t : @prop target),
+          (τ π__s) ⊆ π__t <-> π__s ⊆ (σ π__t).
+
+
+Definition monotone {l1 l2 : level} (f : @prop l1 -> @prop l2) : Prop :=
+  forall π1 π2: @prop l1, (π1 ⊆ π2) -> (f π1 ⊆ f π2). 
+
+Lemma Galois_equiv  (σ : @prop target -> @prop source)
+                    (τ : @prop source -> @prop target) : 
+
+  Adjunction_law σ τ <-> ( monotone σ /\ monotone τ /\ Galois_cp σ τ). 
+Admitted.   
+
+  
 Theorem σ_τ_equiv (σ : @prop target -> @prop source)
                   (τ : @prop source -> @prop target) :
   (Galois_cp σ τ) -> (σRTP σ) <-> (τRTP τ).
@@ -212,32 +229,80 @@ Qed.
 (** *Subset-Closed *)
 (******************************************************************************)
 
+Lemma rhsat_upper_closed {k : level} {K : @language k}:
+  forall (P : par K) (H1 H2 : @hprop k), rhsat P H1 -> H1 ⊆ H2 -> rhsat P H2.  
+Proof. by firstorder. Qed. 
+
 Definition tilde_RSCHC :=
   forall P Ct, exists Cs,
       (forall t, sem (plug (P↓) Ct) t -> (exists s, rel s t /\ sem (plug P Cs) s)).
 
-Definition σh (σ : @prop target -> @prop source) (H : @hprop target) : @hprop source :=
-  fun (b : @prop source) => exists bt, (H bt) /\ b = σ bt.  
+Definition lift {l1 l2 : level} (f : @prop l1 -> @prop l2) (H : @hprop l1) : @hprop l2 :=
+  fun (b1 : @prop l2) => exists b2, (H b2) /\ b1 = f b2. 
 
-Definition σRhP (σ : @hprop target -> @hprop source)
+Definition σRhP (hσ : @hprop target -> @hprop source)
                 (P : par src) (H : @hprop target) :=
-  rhsat P (σ H) -> rhsat (P↓) H.
+  rhsat P (hσ H) -> rhsat (P↓) H.
 
-Definition τh (τ : @prop source -> @prop target) (H : @hprop source) : @hprop target :=
-  fun (b : @prop target) => exists bs, (H bs) /\ b = τ bs.
-
-Definition τRhP (τ : @hprop source -> @hprop target) 
+Definition τRhP (hτ : @hprop source -> @hprop target) 
                 (P : par src) (H : @hprop source) :=
-  rhsat P H -> rhsat (P↓) (τ H).
+  rhsat P H -> rhsat (P↓) (hτ H).
  
-Check (τh τ').
+Check (lift τ').
 Check sCl.
-Check (sCl ∘ (τh τ')). 
+Check (sCl ∘ (lift τ')).
+
+Lemma Galois_fst_SSCH  (σ : @prop target -> @prop source)
+                       (τ : @prop source -> @prop target) :
+  Adjunction_law σ τ ->
+  (forall H : @hprop target, SSC H -> (sCl ∘ (lift τ)) ((sCl ∘ (lift σ)) H) ⊆ H). 
+Proof.
+  move => H_adj H H_ssc b τ_σ_H_b.  
+  unfold sCl, lift in τ_σ_H_b. 
+  destruct τ_σ_H_b as [b' [[b2 [cond b'_eq]] b_b']]. subst. 
+  destruct cond as [b' [[b2' [H_b2' b'_eq]] b2_b']]. subst.  
+  move/H_adj: b2_b' => b2_b'. have b_b2': b ⊆ b2' by firstorder.     
+  by apply: (H_ssc b2'). 
+Qed.
+
+Lemma Galois_snd_SSCH  (σ : @prop target -> @prop source)
+                       (τ : @prop source -> @prop target) :
+  Adjunction_law σ τ ->
+  (forall H : @hprop source, SSC H -> H ⊆ (sCl ∘ (lift σ)) ((sCl ∘ (lift τ)) H)).
+Proof.
+   move => H_adj H H_ssc b H_b.  
+   unfold sCl, lift. exists (σ (τ b)). split.
+   - exists (τ b). split; auto.
+     + exists (τ b). split; auto.
+       ++ by exists b.   
+     + move/Galois_equiv: H_adj. move => [H_mono1 [H_mono2 [G1 G2]]]. 
+       apply: G1. 
+Qed.
+
+Theorem τRSCHP_σRSCHP  (σ : @prop target -> @prop source)
+                       (τ : @prop source -> @prop target) :
+  Adjunction_law σ τ ->
+  ((forall P (H: @hprop target), SSC H ->  σRhP (sCl ∘ (lift σ)) P H) <->
+   (forall P (H: @hprop source), SSC H ->  τRhP (sCl ∘ (lift τ)) P H)).
+Proof.
+  move => H_adj. split.
+  - move => Hσ P H H_ssc rsat_source. 
+    have rsat_sigma_tau : rhsat P ((sCl ∘ (lift σ)) ((sCl ∘ (lift τ)) H)).  
+    { apply: rhsat_upper_closed. exact rsat_source.
+      by apply: Galois_snd_SSCH. }       
+    have τ_ssc : SSC (sCl ((lift τ) H)) by apply: sCl_SCH.
+    exact (Hσ P ((sCl ((lift τ) H))) τ_ssc rsat_sigma_tau).  
+  - move => Hτ P H H_ssc rsat_source.
+    have h_ssc: SSC ((sCl ∘ lift σ) H) by apply: sCl_SCH.
+    move: (Hτ P ((sCl ∘ lift σ) H) h_ssc rsat_source).
+    move => HHH. eapply rhsat_upper_closed. exact HHH.
+    by apply: Galois_fst_SSCH.
+Qed.       
 
 Theorem tilde_RSCHC_τRSCHP :
   (total_rel rel) ->
   (Galois_snd σ' τ') ->
-  tilde_RSCHC <-> (forall P (H: @hprop source), SSC H ->  τRhP (sCl ∘ (τh τ')) P H). 
+  tilde_RSCHC <-> (forall P (H: @hprop source), SSC H ->  τRhP (sCl ∘ (lift τ')) P H). 
 Proof.
   move => Hrel G2. split. 
   - move => Htilde P H H_ssc. unfold τRhP.
@@ -247,4 +312,25 @@ Proof.
     exists (τ' (beh (plug P Cs))). split.
     + by exists (beh (plug P Cs)). 
     + move => t Ht. destruct (HH t Ht) as [s [Hs1 Hs2]]. now exists s.
-  - Admitted. (* TODO *)
+  - move => Hτ P Ct.
+    have Hssc : SSC (fun b: @prop source =>  ~ beh(plug (P↓) Ct) ⊆ τ' b).
+    { move => b1 Hb1 b2 b1_b2 Hb2. apply: Hb1. firstorder. }    
+    specialize (Hτ P (fun b: @prop source =>  ~ beh(plug (P↓) Ct) ⊆ τ' b) Hssc).
+    apply contra in Hτ. move: Hτ. rewrite !neg_rhsat. 
+    move => [Cs Hbeh]. move/NNPP: Hbeh. 
+    exists Cs => t Hsem_t. move: (Hbeh t Hsem_t). firstorder.
+    move => Hf. specialize (Hf Ct).
+    destruct Hf as [b' [Hb' Hbb]]. destruct Hb' as [bs [Hbs1 Hbs2]]. now subst.  
+Qed.
+
+(* CA: looking also for a direct proof here we need the adjunction law,
+        in particular we need monotonicity *)
+Theorem tilde_RSCHC_σRSCHP :
+  Adjunction_law σ' τ' ->
+  tilde_RSCHC <-> (forall P (H: @hprop target), SSC H ->  σRhP (sCl ∘ (lift σ')) P H).
+Proof.
+  move => H_adj. rewrite (τRSCHP_σRSCHP H_adj).
+  move/Galois_equiv: H_adj => [mono_tau [mono_sigma [G1 G2]]].
+  have h_rel : total_rel rel by apply: Galois_implies_total_rel. 
+  by rewrite tilde_RSCHC_τRSCHP. 
+Qed.     
