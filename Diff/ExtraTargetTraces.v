3b37e0d4d549bfe188dca5f9e5ed919a95b79bd3
Rob Blanco <roberto.blanco@inria.fr>
2019-06-15 15:12:52 +0200

    Reorganize "target++" language core
    In this particular setting of two essentially identical languages, in
    which the target has the ability to generate additional events, it will
    make things easier to organize both languages as specializations of a
    shared core.
    
    In addition to this, function calls will be made part of core
    expressions and paired with some additional restrictions expressed
    externally -- some of which were already envisioned in any case. This
    flat vision further simplifies recursive definitions, which otherwise
    can become technically more cumbersome, as the layers need to be
    combined and substructural properties bookkept.

--- Diff/ExtraTargetTraces.v
+++ Diff/ExtraTargetTraces.v
@@ -22,42 +22,50 @@ End DecidableString.
 
 Module StringMap : WS := Make DecidableString.
 
-(* Base expressions. *)
-Inductive expr : Set :=
-| Const : nat -> expr
-| Plus  : expr -> expr -> expr
-| Times : expr -> expr -> expr
-| IfLe  : expr -> expr -> expr -> expr -> expr.
+(* Shared core of source and target languages. For reuse, it seems easiest that
+   they assemble the following components only very slightly differently. *)
+Section Core.
 
-(* Source language. *)
-Module Src.
+  (* Base expressions.
+       RB: An observable effect sequenced with a follow-up expression is added to
+     the core and allowed selectively. Without let bindings, this is not very
+     convenient to use, but suffices for a first approximation.
+       It seems reasonable to treat functions (name and argument) at this level
+     rather than separately. Local restrictions need to be enforced anyway, so
+     a separate level makes little difference.
+       One can abuse Coq's computational mechanism to an extent, but not to the
+     point that the semantics cannot see (relevant) function calls. *)
+  Inductive expr : Set :=
+  | Const : nat -> expr
+  | Plus  : expr -> expr -> expr
+  | Times : expr -> expr -> expr
+  | IfLe  : expr -> expr -> expr -> expr -> expr
+  | Out   : expr -> expr -> expr
+  | Fun   : string -> expr -> expr.
 
-  (* Simple expressions and function types.
-     RB: At the moment, functions cannot call other functions! *)
-  Definition expr_fun : Set := expr.
-  Definition funType : Set := expr_fun -> expr_fun.
-
-  (* Program expressions. *)
-  Inductive expr_par : Set :=
-  | Expr : expr -> expr_par
-  | Fun  : string -> expr_par -> expr_par.
+  (* Function types.
+       RB: At the moment, functions should not call other functions! *)
+  Definition funType : Set := expr -> expr.
 
   (* Programs and contexts (as records, Type and not Set). *)
   Record par :=
     {
       par_funs : StringMap.t funType;
-      par_main : expr_par
+      par_main : expr;
+      par_wf   : False (* TODO: Add well-formedness conditions. *)
     }.
 
   Record ctx :=
     {
-      ctx_funs : StringMap.t funType
+      ctx_funs : StringMap.t funType;
+      ctx_wf   : False (* TODO: Add well-formedness conditions. *)
     }.
 
   Record prg := (* Type, not Set. *)
     {
       prg_funs : StringMap.t (turn * funType);
-      prg_main : expr_par
+      prg_main : expr;
+      prg_wf   : False (* TODO: Add well-formedness conditions. *)
     }.
 
   (* Linking. Assume main in the program and combine functions from program and
@@ -73,17 +81,43 @@ Module Src.
   Definition link (p : par) (c : ctx) : prg :=
     Build_prg
       (StringMap.map2 link_fun (par_funs p) (ctx_funs c))
-      (par_main p).
+      (par_main p)
+      (par_wf p). (* TODO *)
 
   Fail Build_Language link.
 
   (* Traces as evaluation results. *)
   Inductive trace :=
+  | Output : nat -> trace -> trace
   | Result : nat -> trace.
 
   (* Evaluation-based semantics. *)
-  Definition eval (p : prg) : nat := 0. (* TODO *)
+  Definition eval_prg (p : prg) : nat :=
+    let fix eval e :=
+        match e with
+        | Const n => n
+        | Plus e1 e2 => eval e1 + eval e2
+        | Times e1 e2 => eval e1 * eval e2
+        | IfLe ecomp1 ecomp2 ethen eelse =>
+          if eval ecomp1 <=? eval ecomp2 then eval ethen else eval eelse
+        | Out _ e => eval e (* TODO *)
+        | Fun id arg => 0 (* TODO *)
+        end
+    in
+    eval (prg_main p).
 
   Inductive sem : prg -> trace -> Prop :=
-  | SemEval : forall p n, eval p = n -> sem p (Result n).
-End Src.
+  | SemEval : forall p n, eval_prg p = n -> sem p (Result n).
+End Core.
+
+(* Source language. *)
+Module Source.
+End Source.
+
+(* Target language. *)
+Module Target.
+End Target.
+
+(* Compiler. *)
+Section Compiler.
+Section Compiler.
