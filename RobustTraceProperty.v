b8c0126027ea9cfe92a0de176e8ac03479c1d505
Carmine Abate <carmine.abate@studenti.unitn.it>
2019-10-30 10:46:08 +0100

    Coqed generalization of Theorem mentioned in 5.2

--- RobustTraceProperty.v
+++ RobustTraceProperty.v
@@ -115,4 +115,67 @@ Section RobustPreservation.
       by apply: G_σRTP_iff_τRTP.
   Qed.
 
+  Section General.
+    
+    Local Definition hprop__S : Type := hprop trace__S.
+    Local Definition hprop__T : Type := hprop trace__T. 
+    
+    Variable X : hprop__S.
+    Variable Y : hprop__T.
+    
+    Variable α : prop__S -> prop__T. 
+    Variable γ : prop__T -> prop__S.
+
+    Variable αXY : forall π__S, X π__S -> Y (α π__S).
+    Variable γYX : forall π__T, Y π__T -> X (γ π__T).
+
+    Hypothesis γY_mono : forall y1 y2, Y y1 -> Y y2 -> y1 ⊆ y2 -> (γ y1) ⊆ (γ y2).  
+
+    Hypothesis Galois_fst_X_Y :
+      forall π__S, X π__S ->  π__S ⊆ γ (α π__S).
+    
+    Hypothesis Galois_snd_X_Y :
+      forall π__T, Y π__T -> α (γ π__T) ⊆ π__T. 
+
+    Definition γRP (P : par__S) (π__T : (trace__T -> Prop)) :=
+    rsat__S P (γ π__T) -> rsat__T (P ↓) π__T.
+
+    Definition γRYP := forall P π__T, Y π__T -> γRP P π__T.
+
+    Definition αRP (P : par__S) (π__S : prop__S) :=
+    rsat__S P π__S -> rsat__T (P ↓) (α π__S).
+
+    Definition αRXP := forall P π__S, X π__S -> αRP P π__S.
+
+    Lemma γ_α_γ (π__T : prop__T) (Yπ__T : Y π__T): γ (α (γ π__T)) = (γ π__T).
+    Proof.  
+      apply: mutual_inclusion. 
+      - apply: γY_mono.
+          by apply: (αXY (γYX Yπ__T)).  
+          exact: Yπ__T.
+          by apply: Galois_snd_X_Y. 
+      - apply: Galois_fst_X_Y.
+          by apply: γYX.
+    Qed.         
+          
+    Theorem Generalization_σRTP_iff_τRTP : γRYP <-> αRXP.
+    Proof.
+      split.
+      - move => HγRYP P π__S Xπ__S Hsrc.
+        have γHsrc : rsat__S P (γ (α π__S)).
+        { apply: rsat_upper_closed. exact: Hsrc.
+          by apply: Galois_fst_X_Y. } 
+        move: (HγRYP P (α π__S) (αXY Xπ__S) γHsrc).
+        by [].   
+      - move => HαRXP P π__T Yπ__T Hsrc.
+        have Hcancel : γ (α (γ π__T)) = γ π__T by apply: γ_α_γ. 
+        move: (γYX (αXY (γYX Yπ__T))).
+        rewrite Hcancel => Xγπ__T. 
+        move: (HαRXP P (γ π__T) Xγπ__T Hsrc) => Htrg.
+        apply: rsat_upper_closed. exact: Htrg. 
+        by apply: Galois_snd_X_Y. 
+     Qed.
+    
+  End General.
+
 End RobustPreservation.
