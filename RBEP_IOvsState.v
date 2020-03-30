054e15fc58331d0541c9b2373c09cca3c4313c79
Jérémy Thibault <jeremy.thibault@inria.fr>
2020-03-30 14:13:18 +0200

    Define our conjectures and show a few relations between them

--- RBEP_IOvsState.v
+++ RBEP_IOvsState.v
@@ -1,3 +1,4 @@
+Require Import Setoid.
 Set Implicit Arguments.
 
 (* Boolean expressions with infinite loops and  IO
@@ -119,19 +120,15 @@ CoInductive steval : (stream bool) -> bexp -> (trace output) -> Prop :=
 | StEvalOutput : forall s be t, steval s be t -> steval s (Output be) (snoc_trace t (oout event))
 | StEvalOForever : forall s be t, steval s be t -> steval s (OForever be) (snoc_trace t oforever).
 
-
-CoInductive seval : (stream bool) -> bexp -> (trace output) -> (option (stream bool)) -> Prop :=
-| SEvalVal : forall s b, seval s (Val b) (Done b) (Some s)
+CoInductive seval : (stream bool) -> bexp -> (trace output) -> (stream bool) -> Prop :=
+| SEvalVal : forall s b, seval s (Val b) (Done b) s
 | SEvalNot : forall s be t s', seval s be t s' -> seval s (Not be) (map_trace negb t) s'
 | SEvalAnd : forall s be1 t1 s1 be2 t2 s2,
-         seval s be1 t1 (Some s1) -> seval s1 be2 t2 s2 ->
+         seval s be1 t1 s1 -> seval s1 be2 t2 s2 ->
          seval s (And be1 be2) (append_traces andb t1 t2) s2
-| SEvalAndLoop : forall s be1 t1 be2,
-         seval s be1 t1 None ->
-         seval s (And be1 be2) t1 None
-| SEvalInput : forall b s, seval (SCons b s) Input (More OInput (Done b)) (Some s)
+| SEvalInput : forall b s, seval (SCons b s) Input (More OInput (Done b)) s
 | SEvalOutput : forall s be t s', seval s be t s' -> seval s (Output be) (snoc_trace t (oout event)) s'
-| SEvalOForever : forall s be t s', seval s be t s' -> seval s (OForever be) (snoc_trace t oforever) None.
+| SEvalOForever : forall s be t s', seval s be t s' -> seval s (OForever be) (snoc_trace t oforever) s'.
 
 Definition steval' (s:stream bool) (b:bexp) (t:trace output) := exists s', seval s b t s'.
 
@@ -142,25 +139,68 @@ CoInductive equal_traces {a:Type} : trace a -> trace a -> Prop :=
 | EqDone : forall b, equal_traces (Done b) (Done b)
 | EqMore : forall x t1 t2, equal_traces t1 t2 -> equal_traces (More x t1) (More x t2).
 
+Lemma equal_traces_refl {a : Type} : forall (t : trace a), equal_traces t t.
+Proof.
+  cofix Hfix.
+  destruct t.
+  constructor.
+  constructor. apply Hfix.
+Qed.
+
+Lemma equal_traces_map {a : Type} : forall f (t1 t2 : trace a), equal_traces t1 t2 -> equal_traces (map_trace f t1) (map_trace f t2).
+Proof.
+  cofix Hfix.
+  intros f t1 t2 H.
+  inversion H.
+  - apply equal_traces_refl.
+  - subst. admit.
+Admitted.
+
+Lemma equal_traces_append {a : Type} : forall f (t1 t2 t3 t4 : trace a),
+    equal_traces t1 t2 ->
+    equal_traces t3 t4 ->
+    equal_traces (append_traces f t1 t3) (append_traces f t2 t4).
+Proof.
+  admit.
+Admitted.
+
 Lemma steval_deterministic : forall s be t1 t2, steval' s be t1 -> steval' s be t2 -> equal_traces t1 t2.
   cofix H. intros s be t1 t2 [s1 H1] [s2 H2]. inversion H1; inversion H2; subst.
   - inversion H7; subst. apply EqDone.
 (* will get stuck for Not case *)
 Abort. (* TODO: try, maybe also try induction on bexp *)
 
+CoInductive equal_stream : stream bool -> stream bool -> Prop :=
+| eq_scons : forall x s1 s2,
+    equal_stream s1 s2 ->
+    equal_stream (SCons x s1) (SCons x s2)
+.
+
+Lemma equal_stream_refl : forall s, equal_stream s s.
+Proof.
+  cofix Hfix. destruct s; constructor; auto.
+Qed.
+
+Lemma seval_deterministic : forall be s t1 t2 s1 s2,
+    seval s be t1 s1 ->
+    seval s be t2 s2 ->
+    equal_traces t1 t2 /\ equal_stream s1 s2.
+Proof.
+Admitted.
+(* Proof. *)
+(*   induction be; intros s t1 t2 s1 s2 H1 H2; inversion H1; inversion H2; subst. *)
+(*   - split. now constructor. apply equal_stream_refl. *)
+(*   - split. apply equal_traces_map. *)
+(*     edestruct IHbe with (t1 := t) (t2 := t0); eauto. *)
+(*     edestruct IHbe with (t1 := t) (t2 := t0); eauto. *)
+(*   - edestruct (IHbe1 _ _ _ _ _ H4 H11) as [Heqt1 Heqs1]. *)
+
 Lemma steval_deterministic : forall be s t1 t2, steval' s be t1 -> steval' s be t2 -> equal_traces t1 t2.
 Proof.
-  induction be; intros s t1 t2 [s1 H1] [s2 H2]; inversion H1; inversion H2; subst.
-  - now constructor.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
-Admitted.
+  intros be s t1 t2 [s1 H1] [s2 H2].
+  destruct (seval_deterministic H1 H2).
+  assumption.
+Qed.
 
 Lemma steval_defined : forall be s, exists t, steval' s be t.
 Admitted.
@@ -169,3 +209,118 @@ Admitted.
    - functional choice can do it only from steval_defined
      https://coq.github.io/doc/master/stdlib/Coq.Logic.IndefiniteDescription.html
 *)
+
+CoFixpoint proj_output (t : trace event) : trace output :=
+  match t with
+  | Done b => Done b
+  | More (EInput _) t' => More OInput (proj_output t')
+  | More (EOutput x) t' => More (OOutput x) (proj_output t')
+  end.
+
+CoFixpoint stutter (b : bool) : stream bool :=
+  SCons b (stutter b).
+
+Definition proj_input (t : trace event) : stream bool.
+  revert t.
+  cofix proj_input.
+  intros t.
+  exact match t with
+  | Done b => stutter true
+  | More (EInput b) t' => SCons b (proj_input t')
+  | More (EOutput _) t' => proj_input t'
+  end.
+Admitted.
+
+Definition EE := forall be t,
+    ndeval be t <-> steval (proj_input t) be (proj_output t).
+
+Conjecture lang_is_EE : EE.
+
+Axiom inp_no_stutter : trace event -> list bool.
+Fixpoint sapp {a : Type} (l : list a) (s : stream a) : stream a :=
+  match l with
+  | nil => s
+  | cons x l' => SCons x (sapp l' s)
+  end.
+
+Axiom input_no_stutter_proj_input : forall t,
+    sapp (inp_no_stutter t) (stutter true) = proj_input t.
+
+Axiom steval_input_no_stutter : forall be t junk_in junk_in',
+    steval (sapp (inp_no_stutter t) junk_in) be (proj_output t) <->
+    steval (sapp (inp_no_stutter t) junk_in') be (proj_output t).
+
+Definition EE1 := forall be t,
+    ndeval be t ->
+    forall junk_in,
+      steval (sapp (inp_no_stutter t) junk_in) be (proj_output t).
+
+Definition EE2 := forall be t junk_in,
+    steval (sapp (inp_no_stutter t) junk_in) be (proj_output t) ->
+    ndeval be t.
+
+Lemma EE_EE1_EE2 : EE <-> (EE1 /\ EE2).
+Proof.
+  unfold EE, EE1, EE2.
+  split.
+  - intros H.
+    split.
+    + intros be t H0 junk_in.
+      eapply steval_input_no_stutter.
+      rewrite input_no_stutter_proj_input.
+      now apply H.
+    + intros be t junk_in H0.
+      eapply H.
+      erewrite <- input_no_stutter_proj_input.
+      eapply steval_input_no_stutter.
+      eassumption.
+  - intros [H1 H2] be t.
+    split.
+    + intros H.
+      rewrite <- input_no_stutter_proj_input.
+      now eapply H1.
+    + intros H.
+      eapply H2.
+      now rewrite input_no_stutter_proj_input.
+Qed.
+
+CoFixpoint merge ti to :=
+  match to with
+  | Done b => Done b
+  | More OInput to' =>
+    match ti with
+    | SCons i ti' => More (EInput i) (merge ti' to')
+    end
+  | More (OOutput o) to' => More (EOutput o) (merge ti to')
+  end.
+
+Lemma conjecture2 : forall be t,
+    ndeval be t <-> (exists ti to, t = merge ti to /\ steval ti be to).
+Proof.
+  intros.
+  split.
+  - intros H.
+    exists (sapp (inp_no_stutter t) (stutter true)).
+    exists (proj_output t).
+    split.
+    + admit. (* This should be a property of the definition *)
+    + rewrite input_no_stutter_proj_input.
+      (* use EE now *)
+      now apply lang_is_EE.
+Admitted.
+
+Lemma conjecture1 : forall be1 be2,
+    (forall t, ndeval be1 t <-> ndeval be2 t) <->
+    (forall t,
+        (exists ti1 to1, t = merge ti1 to1 /\ steval ti1 be1 to1) <->
+        (exists ti2 to2, t = merge ti2 to2 /\ steval ti2 be2 to2)).
+Proof.
+  intros be1 be2.
+  split.
+  - intros H t.
+    rewrite <- 2!conjecture2.
+    now apply H.
+  - intros H t.
+    rewrite -> 2!conjecture2.
+    now apply H.
+Qed.
